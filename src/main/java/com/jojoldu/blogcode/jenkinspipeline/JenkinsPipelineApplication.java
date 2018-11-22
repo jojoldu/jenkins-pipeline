adf3b2b92d238bc1142daec2bd40dec7467cea76
jojoldu <jojoldu@gmail.com>
2018-11-23 08:02:04 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/blogcode/jenkinspipeline/JenkinsPipelineApplication.java
@@ -0,0 +1,12 @@
+package com.jojoldu.blogcode.jenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPipelineApplication.class, args);
+    }
+}
